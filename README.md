199661f8bddb8e83205a747c2e21510750d55ca9
MajitoCan <mariajose.can23@gmail.com>
2024-09-02 13:58:25 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_majo_ceh
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_majo_ceh/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_majo_ceh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
