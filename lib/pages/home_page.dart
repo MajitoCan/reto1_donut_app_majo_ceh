8dc90cc767e06dddc716058c08833bb28fb447b4
MajitoCan <mariajose.can23@gmail.com>
2024-09-05 15:23:57 -0600

    Añadiendo texto principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,9 +11,44 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.transparent,
-      leading: Icon(Icons.menu, color: Colors.grey[800]),
-    ));
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 24.0),
+            child: IconButton(
+              icon: Icon(Icons.menu, color: Colors.grey[800], size: 36),
+              onPressed: () {
+                print("object");
+              },
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(left: 24.0),
+              child: Icon(
+                Icons.person,
+                color: Colors.grey[800],
+                size: 36,
+              ),
+            )
+          ]),
+      body: const Column(children: [
+        //Texto a want to eat
+        Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text('I want to', style: TextStyle(fontSize: 32)),
+              Text('Eat',
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline)),
+            ],
+          ),
+        ),
+        //tab bar view
+      ]),
+    );
   }
 }
